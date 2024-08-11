15bcf24c5a7f22332bf1888cf20a21bd5cd10659
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-08-11 15:52:34 -0600

    feat(hello_omp): completed [ex 1]

---
+++ hello_omp.c
@@ -0,0 +1,31 @@
+/*
+@author: Samuel Chamalé
+@description: Program that prints a message from each thread in the parallel region.
+@date: 08-11-2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    int nthreads = 10; // Default number of threads
+
+    // If the number of threads is provided from the command line, we use it
+    if (argc > 1) {
+        nthreads = atoi(argv[1]);
+    }
+
+    // Stablish the number of threads
+    omp_set_num_threads(nthreads);
+
+    // Parallelize the section where we print from each thread
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int total_threads = omp_get_num_threads();
+        printf("Hello from thread %d of %d!\n", thread_id, total_threads);
+    }
+
+    return 0;
+}
